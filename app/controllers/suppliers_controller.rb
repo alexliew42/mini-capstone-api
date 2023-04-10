cc4007eb3812ace93f16c6cb2f56b330f64d32d8
Alex Liew <liewtwoo42@gmail.com>
2023-04-10 15:12:15 -0400

    add create, show and index actions

---
+++ app/controllers/suppliers_controller.rb
@@ -0,0 +1,26 @@
+class SuppliersController < ApplicationController
+  def index
+    @suppliers = Supplier.all
+    render :index
+  end
+  
+  def show
+    @supplier = Supplier.find_by(id: params[:id])
+    render :show
+  end
+
+  def create
+    @supplier = Supplier.new(
+      name: params[:name], 
+      email: params[:email], 
+      phone_number: params[:phone_number]
+    )
+    @supplier.save
+    render :show
+  end
+
+  def update
+    @supplier = Supplier.find_by(id: params[:id])
+    @supplier.update
+  end
+end
